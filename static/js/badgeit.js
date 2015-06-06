b9def36fed415b1e3fdb4ee1bd199a36242b8d17
jim <jim@jimsaunders.net>
2015-06-06 03:45:12 -0400

    Updated creation page

--- static/js/badgeit.js
+++ static/js/badgeit.js
@@ -1,44 +1,51 @@
 $(function(){
-  var canvas = new fabric.Canvas('c1');
-  var active = null;
-  var symbolCount = 0;
-  var defaults = {
-  	fill: 'rgba(255,255,255,0.0)',
-	top: canvas.getHeight() / 2,
-	left: canvas.getWidth() / 2,
-	stroke: '#fff',
-	borderColor: 'rgba(255,255,255,0.75)',
-	cornerColor: 'rgba(255,255,255,0.75)',
-	cornerSize: 12,
-	transparentCorners: true
-  };
+    var canvas = new fabric.Canvas('c1');
+    var active = null;
+    var symbolCount = 0;
+    var $canvas = $('.canvas');
 
-  $(document).keyup(function(e)
-  {
-  	var del = (e.keyCode == 46 || e.keyCode == 8);
-  	if (active && del) {
-      active.remove();
-  	  active = null;
-  	  canvas.discardActiveObject();
-  	  symbolCount--;
-  	}
-  });
+    var defaults = {
+        fill: 'rgba(255,255,255,0.0)',
+        top: canvas.getHeight() / 2,
+        left: canvas.getWidth() / 2,
+        stroke: '#fff',
+        borderColor: 'rgba(255,255,255,0.75)',
+        cornerColor: 'rgba(255,255,255,0.75)',
+        cornerSize: 12,
+        transparentCorners: true
+    };
 
-  canvas.on('object:selected', function(e) {active = e.target;});
+    var size = $canvas.width() > $canvas.height() ? $canvas.width() : $canvas.height();
+    $('.tools').innerHeight(size - 8);
+    canvas.setHeight(size);
+    canvas.setWidth(size);
 
-  $('.circle').click(function(e) {
-  	var shapeDefaults = $.extend({}, defaults);
-    var circle = new fabric.Circle($.extend(shapeDefaults, {
-    	radius: $(this).data('drawRadius'),
-    	strokeWidth: $(this).data('drawRadius') * 0.2,
-    	top: defaults.top - $(this).data('drawRadius'),
-    	left: defaults.left - $(this).data('drawRadius')
-    }));
-    canvas.add(circle);
-    canvas.setActiveObject(circle);
-    active = circle;
-    symbolCount++;
-  });
+    $(document).keyup(function(e)
+    {
+        var del = (e.keyCode == 46 || e.keyCode == 8);
+        if (active && del) {
+            active.remove();
+            active = null;
+            canvas.discardActiveObject();
+            symbolCount--;
+        }
+    });
+
+    canvas.on('object:selected', function(e) {active = e.target;});
+
+    $('.circle').click(function(e) {
+        var shapeDefaults = $.extend({}, defaults);
+        var circle = new fabric.Circle($.extend(shapeDefaults, {
+            radius: $(this).data('drawRadius'),
+            strokeWidth: $(this).data('drawRadius') * 0.2,
+            top: defaults.top - $(this).data('drawRadius'),
+            left: defaults.left - $(this).data('drawRadius')
+        }));
+        canvas.add(circle);
+        canvas.setActiveObject(circle);
+        active = circle;
+        symbolCount++;
+    });
 
   $('.square').click(function(e) {
     var shapeDefaults = $.extend({}, defaults);
@@ -89,7 +96,7 @@ $(function(){
   {
   	var data = {
 	  badgeImage: canvas.toDataURL(),
-	  badgeText: $(this).find(':input[name="badge-text"]').val()
+	  badgeText: $(this).find(':input[name="badge-text"]').val().trim()
 	};
 
   	if (symbolCount > 0 && data.badgeText.trim().length > 0) {
